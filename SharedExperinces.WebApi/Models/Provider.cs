636e50d10a39d9af118ee6a6e059ffae13e5f5fb
Khizer Khan <khizer759@gmail.com>
2025-03-10 13:09:29 +0100

    added Provider model

---
+++ SharedExperinces.WebApi/Models/Provider.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedExperinces.WebApi.Models
+{
+	public class Provider
+	{
+
+		[Key]
+		[Required]
+		[MaxLength(8), MinLength(8)]
+		public string CVR { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string Address { get; set; }
+
+		[Required]
+		public string PhoneNumber { get; set; }
+	}
+}
